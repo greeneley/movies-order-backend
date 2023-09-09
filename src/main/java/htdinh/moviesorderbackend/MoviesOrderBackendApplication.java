faae8be76303dfec4d372ceb2663cd2dc7feece0
greeneley <anhchangtoanhoc97@gmail.com>
2023-09-09 17:41:05 +0700

    feat: spring initilizier

---
+++ src/main/java/htdinh/moviesorderbackend/MoviesOrderBackendApplication.java
@@ -0,0 +1,13 @@
+package htdinh.moviesorderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesOrderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesOrderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/htdinh/moviesorderbackend/MoviesOrderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package htdinh.moviesorderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesOrderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
